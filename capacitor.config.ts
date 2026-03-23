e9204a5bcb1dfc5352eeb505cda2e00559f16092
Jonnathan1998 <jjinaguazo@sudamericano.edu.ec>
2026-03-23 10:57:59 -0500

    app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.europeek.app',
   appName: 'EuroPeek',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    url: 'https://englishpeak.duckdns.org',
+    cleartext: false
+  }
 };
 
 export default config;
