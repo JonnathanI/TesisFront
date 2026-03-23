e7f56e9366adac0e1a44d1fc08544c77cce14e7b
Jonnathan1998 <jjinaguazo@sudamericano.edu.ec>
2026-03-23 09:43:46 -0500

    apk

---
+++ android/app/src/main/java/com/europeek/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.europeek.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.europeek.app',
+  appName: 'EuroPeek',
+  webDir: 'build'
+};
+
+export default config;
